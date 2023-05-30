2db2eb953a879b143018a9d1dccb2785a84d9ce8
Alexander <38129787+hedgehog319@users.noreply.github.com>
2023-05-30 20:38:15 +1000

    Add files via upload

--- zedm.py
+++ zedm.py
@@ -1,66 +1,113 @@
 import pyzed.sl as sl
+import numpy as np
 from time import sleep
+
+# img_capture = False
+img_capture = True
+
+pos_tracking = False
+# pos_tracking = True
+
+
+COUNTER_MAX_VALUE = 200
 
 
 def main():
-    zed = sl.Camera()
+    print('Configure init parameters')
 
-    print('configuring')
     init_params = sl.InitParameters()
     init_params.camera_resolution = sl.RESOLUTION.HD720
     init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
     init_params.coordinate_units = sl.UNIT.METER
+    init_params.depth_mode = sl.DEPTH_MODE.NONE
     init_params.camera_fps = 15
 
-    if zed.open(init_params) != sl.ERROR_CODE.SUCCESS:
+    print('Open the camera')
+
+    zed = sl.Camera()
+    zed_status = zed.open(init_params)
+    if zed_status != sl.ERROR_CODE.SUCCESS:
+        print(repr(zed_status))
         exit(1)
 
-    print('init tracking')
-    # Включение обработки позиции
+    print('Configure runtime parameters')
+
+    runtime_params = sl.RuntimeParameters()
+
+    print('Configuring stream parameters')
+
+    stream_params = sl.StreamingParameters()
+    stream_params.codec = sl.STREAMING_CODEC.H264
+    stream_params.bitrate = 4000
+    stream_params.port = 30000
+
+    zed_status = zed.enable_streaming(stream_params)
+    if zed_status != sl.ERROR_CODE.SUCCESS:
+        print(repr(zed_status))
+        exit(1)
+
+    print('Init position tracking')
     py_transform = sl.Transform()
     tracking_parameters = sl.PositionalTrackingParameters(
         _init_pos=py_transform)
-    if zed.enable_positional_tracking(tracking_parameters) != sl.ERROR_CODE.SUCCESS:
+    # zed_status = zed.enable_positional_tracking(tracking_parameters)
+    if zed_status != sl.ERROR_CODE.SUCCESS:
+        print(repr(zed_status))
         exit(1)
 
-    print('init pose and sensor data')
-    # Данные о позиции и данные с датчиков
+    print('Init pose data')
     zed_pose = sl.Pose()
+
+    print('Init sensors data')
     sensors_data = sl.SensorsData()
 
     image = sl.Mat()
+    counter = 0
+
     while True:
-        if zed.grab() != sl.ERROR_CODE.SUCCESS:
+        zed_status = zed.grab(runtime_params)
+        if zed_status != sl.ERROR_CODE.SUCCESS:
+            print(zed_status)
             continue
 
         # Получение изображения
-        # zed.retrieve_image(image, sl.VIEW.LEFT)
-        # timestamp = zed.get_timestamp(sl.TIME_REFERENCE.CURRENT)
-        # print(f"Image resolution: {image.get_width()} x {image.get_height()} || \
-        #     Image timestamp: {timestamp.get_milliseconds()}\n")
+        if img_capture:
+            zed.retrieve_image(image, sl.VIEW.LEFT)
+            timestamp = zed.get_timestamp(sl.TIME_REFERENCE.CURRENT)
+            # print(f"Image resolution: {image.get_width()}x{image.get_height()} || \
+                # Image timestamp: {timestamp.get_milliseconds()}\n")
 
         # Трекинг позиции
-        zed.get_position(zed_pose, sl.REFERENCE_FRAME.WORLD)
-        zed.get_sensors_data(sensors_data, sl.TIME_REFERENCE.IMAGE)
-        zed_imu = sensors_data.get_imu_data()
+        if pos_tracking:
+            zed.get_position(zed_pose, sl.REFERENCE_FRAME.WORLD)
+            zed.get_sensors_data(sensors_data, sl.TIME_REFERENCE.CURRENT)
+            zed_imu = sensors_data.get_imu_data()
 
-        py_translation = sl.Translation()
-        tx = round(zed_pose.get_translation(py_translation).get()[0], 3)
-        ty = round(zed_pose.get_translation(py_translation).get()[1], 3)
-        tz = round(zed_pose.get_translation(py_translation).get()[2], 3)
-        print(f"Translation: {tx=}, {ty=}, {tz=}, \
-              Timestamp: {zed_pose.timestamp.get_milliseconds()}\n")
+            # Смещение относительно точки отчёта
+            py_translation = sl.Translation()
+            tx = round(zed_pose.get_translation(py_translation).get()[0], 3)
+            ty = round(zed_pose.get_translation(py_translation).get()[1], 3)
+            tz = round(zed_pose.get_translation(py_translation).get()[2], 3)
+            print(f"Translation: {tx=}, {ty=}, {tz=}\n")
 
-        # Угловая скорость
-        a_velocity = [0, 0, 0]
-        zed_imu.get_angular_velocity(a_velocity)
-        vx = round(a_velocity[0], 3)
-        vy = round(a_velocity[1], 3)
-        vz = round(a_velocity[2], 3)
-        print(f"IMU Angular Velocity: {vx=}, {vy=}, {vz=}\n")
-        sleep(0.5)
+            # Угловая скорость
+            # a_velocity = np.zeros(3)
+            # zed_imu.get_angular_velocity(a_velocity)
+            a_velocity = zed_imu.get_angular_velocity()
+            vx, vy, vz = np.round(a_velocity, 3)
+            print(f"IMU ang velocity: {vx=}, {vy=}, {vz=}\n")
 
-    # Close the camera
+            # Угловое ускорение
+            # a_accel = np.zeros(3)
+            # zed_imu.get_linear_acceleration(a_accel)
+            a_accel = zed_imu.get_linear_acceleration()
+            ax, ay, az = np.round(a_accel, 3)
+            print(f"IMU ang acceleration: {ax=}, {ay=}, {az=}\n")
+
+    print('Disable streaming')
+    zed.disable_streaming()
+
+    print('Close the camera')
     zed.close()
 
 
